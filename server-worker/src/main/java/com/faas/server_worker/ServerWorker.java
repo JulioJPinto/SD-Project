cf7c0be2eae1536e340f34faa901860744aff4c4
Filipe Pereira <filipe.simoes.pereira2817@gmail.com>
2023-12-22 18:12:58 +0000

    Manager-Worker connection

--- server-worker/src/main/java/com/faas/server_worker/ServerWorker.java
+++ server-worker/src/main/java/com/faas/server_worker/ServerWorker.java
@@ -1,11 +1,11 @@
-package com.faas.client;
+package com.faas.server_worker;
 
 import java.io.*;
 import java.net.Socket;
 
-public class Client {
-    private static final String SERVER_MANAGER_ADDRESS = "localhost"; // 127.0.0.1
-    private static final int SERVER_MANAGER_PORT = 8080;
+public class ServerWorker {
+    private static final String SERVER_MANAGER_ADDRESS = "localhost";
+    private static final int SERVER_MANAGER_PORT = 8081;
 
     public static void main(String[] args) {
         try {
@@ -13,14 +13,17 @@ public class Client {
             DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
             DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 
-            out.writeUTF("Hello from client!");
+            out.writeUTF("Hello from server worker!");
             out.flush();
             System.out.println("Sent message to manager.");
 
             String response = in.readUTF();
             System.out.println("Server response: " + response);
+
             response = in.readUTF();
             System.out.println("Server response: " + response);
+            out.writeUTF("Hello from server worker!");
+            out.flush();
 
             in.close();
             out.close();
