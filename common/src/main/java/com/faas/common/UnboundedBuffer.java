68ce86b7363356fb59538bf1a6b50cf518095b06
Filipe Pereira <filipe.simoes.pereira2817@gmail.com>
2023-12-29 18:03:00 +0000

    More Thread-Safe Structures

---
+++ common/src/main/java/com/faas/common/UnboundedBuffer.java
@@ -0,0 +1,76 @@
+package com.faas.common;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.Condition;
+
+public class UnboundedBuffer<T> {
+    private final Queue<T> queue;
+    private final ReentrantReadWriteLock lock;
+    private final Condition notEmpty;
+
+    public UnboundedBuffer() {
+        this.queue = new ArrayDeque<T>();
+        this.lock = new ReentrantReadWriteLock();
+        this.notEmpty = lock.writeLock().newCondition();
+    }
+
+    public boolean isEmpty() {
+        this.lock.readLock().lock();
+        try {
+            return this.queue.isEmpty();
+        } finally {
+            this.lock.readLock().unlock();
+        }
+    }
+
+    public void produce(T item) {
+        this.lock.writeLock().lock();
+        try {
+            this.queue.add(item);
+            this.notEmpty.signal();
+        } finally {
+            this.lock.writeLock().unlock();
+        }
+    }
+
+    public T consume() throws InterruptedException {
+        this.lock.writeLock().lock();
+        try {
+            while (this.queue.isEmpty()) {
+                this.notEmpty.await();
+            }
+            return this.queue.poll();
+        } finally {
+            this.lock.writeLock().unlock();
+        }
+    }
+
+    public T peek() {
+        this.lock.readLock().lock();
+        try {
+            return this.queue.peek();
+        } finally {
+            this.lock.readLock().unlock();
+        }
+    }
+
+    public int size() {
+        this.lock.readLock().lock();
+        try {
+            return this.queue.size();
+        } finally {
+            this.lock.readLock().unlock();
+        }
+    }
+
+    public void notifyWaiters() {
+        this.lock.writeLock().lock();
+        try {
+            this.notEmpty.signalAll();
+        } finally {
+            this.lock.writeLock().unlock();
+        }
+    }
+}
